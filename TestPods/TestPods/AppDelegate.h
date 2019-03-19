8e70cfe4db898963fcfe5b78dabfa09fb3054190
jiangshunan <568275828@qq.com>
2019-03-19 14:02:07 +0800

    init

---
+++ TestPods/TestPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPods
+//
+//  Created by Miller on 2019/3/19.
+//  Copyright © 2019 Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
